72600e994ea91de85111c921aa8b772014957bbb
Solange Gomez <solangegomez.sg1@gmail.com.ar>
2023-03-01 23:12:13 -0300

    Primer escritura en el repositorio

---
+++ js/events.js
@@ -0,0 +1,7 @@
+document.getElementById('boton_mostrar').addEventListener('click', function () {
+    document.getElementById("demo").style.display = 'block';
+});
+
+document.getElementById('boton_ocultar').addEventListener('click', function () {
+    document.getElementById("demo").style.display = 'none';
+});
